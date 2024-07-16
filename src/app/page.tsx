69de9b8176e9188f28b4d801056647d4cf9a0b59
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-07-16 19:12:08 +0200

    Add route.ts for arcjet Rate limit and change title in Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function HomePage() {
         <div className="container relative pb-10">
           <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
             <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-              Introduce ‎
+              Introducing ‎
             <span className="dark:text-zinc-950 text-white">
               <Highlight>DevLab</Highlight>  
             </span>
